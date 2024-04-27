d37d1e209dad47dc191d529d10727c3627417334
idomshi <56523771+idomshi@users.noreply.github.com>
2024-04-27 16:16:17 +0900

    Add side menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import DropZone from './components/DropZone.vue'
 import PreviewArea from './components/PreviewArea.vue';
-import DownloadButton from './components/DownloadButton.vue'
+import SideMenu from './components/SideMenu.vue'
 import { useSourceCanvas } from './composables/useSourceCanvas';
 
 const { sourceCanvas } = useSourceCanvas()
@@ -10,10 +10,12 @@ const { sourceCanvas } = useSourceCanvas()
 <template>
   <div class="main">
     <div :class="{ 'preview-hidden': sourceCanvas === undefined }" class="preview">
-      <PreviewArea></PreviewArea>
-      <DownloadButton></DownloadButton>
+      <div class="preview-main">
+        <PreviewArea></PreviewArea>
+      </div>
+      <SideMenu></SideMenu>
     </div>
-    <div v-if="sourceCanvas === undefined">
+    <div v-if="sourceCanvas === undefined" class="p-2">
       <DropZone></DropZone>
     </div>
   </div>
@@ -21,7 +23,7 @@ const { sourceCanvas } = useSourceCanvas()
 
 <style scoped>
 .main {
-  @apply w-full h-dvh p-2 flex flex-col gap-2;
+  @apply w-full h-dvh flex flex-col gap-2;
 
   &>div {
     @apply flex-1;
@@ -29,7 +31,11 @@ const { sourceCanvas } = useSourceCanvas()
 }
 
 .preview {
-@apply w-full h-full flex flex-col content-center flex-1;
+  @apply w-full h-full flex flex-row
+}
+
+.preview-main {
+  @apply w-full h-full p-2 flex flex-col content-center flex-1;
 
   &>div {
     @apply flex-auto;
---
+++ src/components/SideMenu.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import DownloadButton from './DownloadButton.vue';
+import { useSourceCanvas } from '../composables/useSourceCanvas';
+const { width, height } = useSourceCanvas()
+</script>
+
+<template>
+    <aside class="sidemenuroot">
+        <section class="">
+            <legend for="width" class="text-sm text-gray-800">width: </legend><input id="width" type="text" v-model="width" class="ml-2 w-20 p-1 text-right border rounded">
+            <legend for="height" class="text-sm text-gray-800">height: </legend><input id="height" type="text" v-model="height" class="ml-2 w-20 p-1 text-right border rounded">
+        </section>
+        <section>
+            <DownloadButton></DownloadButton>
+        </section>
+    </aside>
+</template>
+
+<style scoped>
+.sidemenuroot {
+    @apply p-2 w-fit h-full bg-gray-100 flex flex-col justify-between flex-none;
+}
+</style>
